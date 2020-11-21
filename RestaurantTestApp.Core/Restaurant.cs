430d4dffe0682bcf453220e8b3edf912a31af435
Samuel Duga <sam.duga@gmail.com>
2020-11-21 14:01:45 -0600

    Initial commit

---
+++ RestaurantTestApp.Core/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RestaurantTestApp.Core
+{
+    public class Restaurant
+    {
+        public enum CuisineType
+        {
+            None,
+            Mexican,
+            Italian,
+            Indian,
+            American
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
---
+++ RestaurantTestApp.Data/IRestaurantData.cs
@@ -0,0 +1,37 @@
+﻿using RestaurantTestApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using static RestaurantTestApp.Core.Restaurant;
+
+namespace RestaurantTestApp.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAllRestaurants();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Sam's Pizza Wagon", Location = "Wisconsin", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Cinnabongo", Location = "London", Cuisine = CuisineType.Mexican},
+                new Restaurant {Id = 3, Name = "Saffron Palace", Location = "New York", Cuisine = CuisineType.Indian}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAllRestaurants()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+
+}
